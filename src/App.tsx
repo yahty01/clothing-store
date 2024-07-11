0b8687fc4b053ce70601a80655af55ffa5634d10
Egor Savelev <119876639+yahty01@users.noreply.github.com>
2024-07-12 01:01:12 +0300

    Refactor:   .gitignore - Отключил отслеживание папки .idea   GlobalStyled.ts - добавил цвета для body из theme.ts Add:   theme.ts - добавил переменную с константами всех используемых цветов, для использования в SC пишем ${theme.mainTextColor}, предварительно импортировав в компоненту.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Basket from './pages/basket/Basket';
 import HomePage from './pages/homePage/HomePage';
-import Navigation from './components/Navigator/Navigator';
+import Navigation from './components/navigator/Navigator';
 import { CartProvider } from './context/CartContext';
 
 const App: React.FC = () => {
