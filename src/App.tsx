74e75523238ae1c8eff4ed0b742f777c540dad20
Egor Savelev <119876639+yahty01@users.noreply.github.com>
2024-07-19 05:33:17 +0300

    fit(MainPhoto)

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
 				<Route path="/product/:id"
 				       element={<ProductDetail products={products}/>}/>
 				<Route path="/404"
-				       element={<h1 style={{textAlign: 'center'}}>404:PAGE NOT FOUND</h1>}/>
+				       element={<h1>404:PAGE NOT FOUND</h1>}/>
 				<Route path="*"
 				       element={<Navigate to="/404"/>}/>
 			</Routes>
